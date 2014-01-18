f1f9d8119b47a9e59fa136a3777ef55a8d4d00fe
Ben West <bewest@gmail.com>
2014-01-17 19:20:59 -0800

    Update docs, environment

--- env.js
+++ env.js
@@ -4,7 +4,7 @@ module.exports = (function ( ) {
       // port 6776 if undefined
       port: (process.env.PORT || 6776)
       // type of git backend to use
-    , type: (process.env.GIT_BACKEND || 'fs-db')
+    , type: 'fs-db'
       // base directory to host bare repos in
     , base: (process.env.BASE || './out')
   };
